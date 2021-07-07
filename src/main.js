f5739c670ebbc28103421ffa5afc98838623de92
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-07-07 02:01:08 -0700

    add logic for displaying movies

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Zodiac from './js/astrology-service';
 import Movie from './js/movie-service';
-
+// import axios from 'axios';
 
 // import { Project } from 'js/project';
 let createImg = (sign) => {
@@ -14,6 +14,57 @@ let createImg = (sign) => {
   imgElement.id=`sign${sign}`;
   imgElement.style='float: right; border-radius: 300px; width: 150px';
   return imgElement;
+};
+
+// data.d.i, i is the key in the movie object for image
+// data.d is the array for each movie
+// data.d.l is the title
+let showMovies = (data) => {
+  $('.movieResults').val('');
+  console.log('in showMovies, data', data);
+  data.data.d.forEach((movie) => {
+    if (!movie.y) {
+      const img = movie.i.imageUrl;
+      console.log('img', img);
+      const {l, s, rank} = movie;
+      
+      const movieEl = document.createElement('div');
+      movieEl.classList.add('movie');
+      movieEl.innerHTML = `
+      <img src="${img}" alt="${l}">
+      <div class="movie-info">
+        <h6>${l}</h6>
+        <span class="green">${rank}</span>
+      </div>
+      <div class="overview">
+        <h3>Overview</h3>
+        <p>People: <span id='people'>${s}</span></p>
+        <p>Type: <span id='type'>Actor<span></p>
+      </div>
+      `;
+      $('.movieResults').append(movieEl);
+    } else {
+      const img = movie.i.imageUrl;
+      console.log('img', img);
+      const {l, y, s, q} = movie;
+      
+      const movieEl = document.createElement('div');
+      movieEl.classList.add('movie');
+      movieEl.innerHTML = `
+      <img src="${img}" alt="${l}">
+      <div class="movie-info">
+        <h6>${l}</h6>
+        <span class="green">${y}</span>
+      </div>
+      <div class="overview">
+        <h3>Overview</h3>
+        <p>People: <span id='people'>${s}</span></p>
+        <p>Type: <span id='type'>${q}<span></p>
+      </div>
+      `;
+      $('.movieResults').append(movieEl);
+    }
+  });
 };
 
 $(document).ready(function() {
@@ -43,13 +94,14 @@ $(document).ready(function() {
         $('#number').text(luckyNum);
         $('#time').text(luckyTime);
         $('#mood').text(mood);
-        return Movie.fetchData(color);
+        return Movie.fetchData(mood);
       })
       .then(res => {
         if (res instanceof Error) {
           throw Error(`Movie API error: ${res.message}`);
         }
         console.log('movie api res', res);
+        showMovies(res);
       })
       .catch(err => {
         console.log('in catch', err);
@@ -98,23 +150,23 @@ $(document).ready(function() {
 
 //WIP code below is functionality for the service js files
 // console.log('aztro key', process.env.API_KEY);
-var axios = require("axios").default;
+// var axios = require("axios").default;
 
-var options = {
-  method: 'POST',
-  url: 'https://sameer-kumar-aztro-v1.p.rapidapi.com/',
-  params: {sign: 'cancer', day: 'today'},
-  headers: {
-    'x-rapidapi-key': `${process.env.API_KEY}`,
-    'x-rapidapi-host': 'sameer-kumar-aztro-v1.p.rapidapi.com'
-  }
-};
+// var options = {
+//   method: 'POST',
+//   url: 'https://sameer-kumar-aztro-v1.p.rapidapi.com/',
+//   params: {sign: 'cancer', day: 'today'},
+//   headers: {
+//     'x-rapidapi-key': `${process.env.API_KEY}`,
+//     'x-rapidapi-host': 'sameer-kumar-aztro-v1.p.rapidapi.com'
+//   }
+// };
 
-axios.request(options).then(function (response) {
-  console.log('zodiac', response.data);
-}).catch(function(error) {
-  console.error(error);
-});
+// axios.request(options).then(function (response) {
+//   console.log('zodiac', response.data);
+// }).catch(function(error) {
+//   console.error(error);
+// });
 
 
 // //powerful query
